81b65e24908fc3de1247a8de27de8ec14abe9b1f
yangrongzhao <yangrongzhao@axera-tech.com>
2024-12-26 15:45:10 +0800

    add audio source for whisper

--- whisper/app.py
+++ whisper/app.py
@@ -3,24 +3,24 @@ from ax_whisper import Whisper
 
 model = Whisper(model_path="./models", model_type="small")
 
-def run(input_audio):
+def transcribe(input_audio):
     return model.transcribe(input_audio)
 
 with gr.Blocks() as demo:
     gr.Markdown(
         """
         # Whisper
-        General-purpose speech recognition model from OpenAI.
+        General-purpose speech recognition model from OpenAI.  
+        Deployed on AX650N, visit https://axera-tech.com for product introduction.
         """
     )
     with gr.Group():
         # 上传音频
-        audio = gr.Audio(label="Input audio", type="filepath", sources=["upload"], format="wav")
+        audio = gr.Audio(label="Input audio", type="filepath", sources=["upload", "microphone"], format="wav")
         # 识别结果
         result = gr.Textbox(label="Result", interactive=False)
         # 识别按钮
         run_btn = gr.Button("Run")
-        run_btn.click(fn=run, inputs=[audio], outputs=[result])
+        run_btn.click(fn=transcribe, inputs=[audio], outputs=[result])
 
-
-demo.launch(server_name="0.0.0.0", server_port=8080)
+demo.launch(server_name="0.0.0.0", server_port=8080, ssl_verify=False)
